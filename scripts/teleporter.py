c38ba1adb5a06b11ad81d77d458825c1d7339b1e
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2019-09-04 10:35:03 +0300

    Portal system

--- scripts/teleporter.py
+++ scripts/teleporter.py
@@ -1,15 +1,21 @@
-import random
+import time
 
-from mcpi import block
 from mcpi.minecraft import Minecraft
+from mcpi.vec3 import Vec3
 
 mc = Minecraft.create()
 
-x = random.randint(-32, 32)
-y = 66
-z = random.randint(-32, 32)
+# Portal location
+pos_in = Vec3(222, -9, -68)
+pos_out = Vec3(222, 6, -39)
 
-mc.postToChat("Go to ({}, {}, {})".format(x, y, z))
-me = mc.getPlayerEntityId("_Egor_S_")
-mc.entity.setPos(me, [x, y + 1, z])
-mc.setBlock(x, y, z, block.GLASS.id)
+while True:
+    for player in mc.getPlayerEntityIds():
+        pos = mc.entity.getTilePos(player)
+        print(pos)
+        if pos_in.x - 1 <= pos.x <= pos_in.x + 1:
+            if pos_in.y - 1 <= pos.y <= pos_in.y + 1:
+                if pos_in.z - 1 <= pos.z <= pos_in.z + 1:
+                    mc.postToChat("We need to go deeper!")
+                    mc.entity.setTilePos(player, pos_out)
+    time.sleep(2)
